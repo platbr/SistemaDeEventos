fc32c1c418cb6af0c7388dcf716515555cb1c354
platbr <wagner.caixeta@gmail.com>
2014-05-20 23:31:17 -0300

    Implementa IngressoSAO

---
+++ src/br/ufg/inf/espbd/siseventos/data/IngressoDAOImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufg.inf.espbd.siseventos.data;
+
+import br.ufg.inf.espbd.siseventos.model.Ingresso;
+import java.util.List;
+
+/**
+ *
+ * @author Wagner
+ */
+public class IngressoDAOImpl implements IngressoDAO{
+
+    @Override
+    public void salvar(Ingresso ingresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizar(Ingresso ingresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remover(Ingresso ingresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Ingresso> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
